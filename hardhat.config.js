6a589d40149bf80368760d72912cc8ea9cdd50ee
anton-karlovskiy <antonkarlovskiy@outlook.com>
2023-01-31 02:40:43 +0100

    chore: clean up

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,19 +1,16 @@
-require("@nomiclabs/hardhat-waffle");
+require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config({ path: ".env" });
-require("@nomiclabs/hardhat-etherscan");
 
-const ALCHEMY_API_KEY_URL = process.env.ALCHEMY_API_KEY_URL;
-
-const MUMBAI_PRIVATE_KEY = process.env.MUMBAI_PRIVATE_KEY;
-
+const QUICKNODE_HTTP_URL = process.env.QUICKNODE_HTTP_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 const POLYGONSCAN_KEY = process.env.POLYGONSCAN_KEY;
 
 module.exports = {
   solidity: "0.8.4",
   networks: {
     mumbai: {
-      url: ALCHEMY_API_KEY_URL,
-      accounts: [MUMBAI_PRIVATE_KEY],
+      url: QUICKNODE_HTTP_URL,
+      accounts: [PRIVATE_KEY],
     },
   },
   etherscan: {
